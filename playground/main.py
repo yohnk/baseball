f7d74a78f0d0b6cf7687d17983b95b8bef7436ab
Ryan Yohnk <ryanyohnk@gabon.local>
2022-07-19 13:29:57 -0500

    First attempt at the graph worked but was much slower than normal asyncio. Going to try to get the graph down to similar aio times.

--- playground/main.py
+++ playground/main.py
@@ -11,6 +11,7 @@ import requests
 
 from data.data_types import get_types
 # from playground.TaskGraph import TaskGraph
+from playground.TaskGraph import TaskGraph
 
 
 class APISupplier:
@@ -52,42 +53,38 @@ class PandasCleaner:
         return data_frame.astype(self.types)
 
 
-class DataCache:
-
-    async def aiocache(self, api_data_task):
-        cache_id, data = await api_data_task
-        async with aiofiles.open('/tmp/{}.txt'.format(cache_id), mode='w') as f:
-            await f.write(data)
-
-    def cache(self, api_data_task):
-        cache_id, data = api_data_task
-        with open('/tmp/{}.txt'.format(cache_id), mode='w') as f:
-            f.write(data)
+# class DataCache:
+#
+#     async def aiocache(self, api_data_task):
+#         cache_id, data = await api_data_task
+#         async with aiofiles.open('/tmp/{}.txt'.format(cache_id), mode='w') as f:
+#             await f.write(data)
+#
+#     def cache(self, api_data_task):
+#         cache_id, data = api_data_task
+#         with open('/tmp/{}.txt'.format(cache_id), mode='w') as f:
+#             f.write(data)
 
 
 async def main():
-    game_ids = [661042, 661041, 661040, 661039, 661036, 661038]
+    game_ids = [661042, 661041, 661040, 661039, 661036, 661038] * 10
 
     asp = {game_id: APISupplier(game_id) for game_id in game_ids}
     pc = PandasCreator()
     pcl = PandasCleaner(types=get_types()["statcast"])
-    dc = DataCache()
+    # dc = DataCache()
 
     start = time.time()
 
     df_tasks = []
-    cache_tasks = []
     for game_id in game_ids:
         api = asp[game_id]
         task1 = asyncio.create_task(api.aioget())
         task2 = asyncio.create_task(pc.aiocreate(task1))
         task3 = asyncio.create_task(pcl.aioclean(task2))
         df_tasks.append(task3)
-        task4 = asyncio.create_task(dc.aiocache(task1))
-        cache_tasks.append(task4)
 
     async_df = await asyncio.gather(*df_tasks)
-    await asyncio.gather(*cache_tasks)
 
     print("Async Time", time.time() - start)
 
@@ -99,37 +96,56 @@ async def main():
         api = asp[game_id]
         data = api.get()
         sync_df.append(pcl.clean(pc.create(data)))
-        dc.cache(data)
 
     print("Sync Time", time.time() - start)
 
+
+def main_graph():
+    game_ids = [661042, 661041, 661040, 661039, 661036, 661038] * 10
+
+    asp = {game_id: APISupplier(game_id) for game_id in game_ids}
+    pc = PandasCreator()
+    pcl = PandasCleaner(types=get_types()["statcast"])
+    # dc = DataCache()
+
     graph = TaskGraph()
-    graph.root().async_task(api.aioget).process_task(pc.create).split(
-        graph.process_task(pcl.clean).collect(),
-        graph.async_task(dc.aiocache)
-    )
 
-    result = graph.run()
+    start = time.time()
+
+    for game_id in game_ids:
+        api = asp[game_id]
+        graph.root().async_task(api.aioget).async_task(pc.aiocreate).async_task(pcl.aioclean).collect()
+
+    print(graph.run())
+
+    print("Graph Time", time.time() - start)
+
+    graph.close()
 
 
-async def test(output, f: concurrent.futures.Future):
+async def test(output):
     await asyncio.sleep(1)
-    f.set_result(output)
+    return output
 
-if __name__ == "__main__":
+
+async def wrap(m, f: concurrent.futures.Future):
+    r = await m
+    f.set_result(r)
+
+
+def test_async():
     loop = asyncio.new_event_loop()
     thread = Thread(target=loop.run_forever)
     thread.start()
 
     f1 = concurrent.futures.Future()
-    _ = loop.call_soon_threadsafe(asyncio.ensure_future, test("One", f1))
+    _ = loop.call_soon_threadsafe(asyncio.ensure_future, wrap(test("One"), f1))
     f2 = concurrent.futures.Future()
-    _ = loop.call_soon_threadsafe(asyncio.ensure_future, test("Two", f2))
+    _ = loop.call_soon_threadsafe(asyncio.ensure_future, wrap(test("Two"), f2))
 
     fs = [f1, f2]
 
     while len(fs) > 0:
-        done = len(fs) == 0 or all(fs)
         complete = [f for f in fs if f.done()]
         for f in complete:
             print(f.result())
@@ -144,6 +160,6 @@ if __name__ == "__main__":
     loop.close()
 
 
-    # task = loop.create_task(test())
-
-
+if __name__ == "__main__":
+    # asyncio.run(main())
+    main_graph()
